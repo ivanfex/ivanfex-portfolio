8f37fc2a8e4fe0c479884829896007c4fb3098d7
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP-2.home>
2018-08-20 00:30:20 -0400

    feat(InfoBlock): expand info message on block

--- browser/pages/InfoBlock.js
+++ browser/pages/InfoBlock.js
@@ -6,6 +6,8 @@ class InfoBlock extends Component {
 			<section className="info-block blocky-block">
 				<div className="text-wrapper">
 					<h1 className="text">I develop web apps with JavaScript, create games with C#, and make little things that move || control with Arduino && Raspberry Pi</h1>
+					<br/>
+					<h1 className="text">I love telling stories through interactive media</h1>
 				</div>
 			</section>
 		)
