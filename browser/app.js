dfc03fae6012a7ad36ae344597c90af97b2d2e10
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP-2.home>
2018-08-19 23:56:22 -0400

    feat(styles): add main styles for app

--- browser/app.js
+++ browser/app.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import './styles/main.css'
+import Main from './Main';
 
 ReactDOM.render(
 	<Provider store = { store }>
-		<div>Hello, world!</div>
+		<Main/>
 	</Provider>,
 	document.getElementById('app')
 )
