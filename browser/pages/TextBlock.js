abf403bb916f0b3fe9c9f320f108057d1950d129
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP-2.home>
2018-09-12 14:04:06 -0400

    feat(TextBlock): add reshake text to main block

--- browser/pages/TextBlock.js
+++ browser/pages/TextBlock.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { ShakeSlow } from 'reshake'
 
 class TextBlock extends Component {
 	render(){
@@ -6,7 +7,7 @@ class TextBlock extends Component {
 			<section className="text-block blocky-block">
 				<div className="text-wrapper">
 					<br/>
-					<h1 className="text">I’m Ivan Felix, a full stack developer and maker driven by curiosity</h1>
+					<h1 className="text">I’m Ivan Felix, a full stack <ShakeSlow className="shaky" fixed={true}><span className="underlined underlined--thick">developer</span></ShakeSlow> and <ShakeSlow className="shaky" fixed={true}><span className="underlined underlined--thick">maker</span></ShakeSlow> driven by curiosity</h1>
 					<br/>
 					<h1 className="text">Currently making web apps at 718 Digital</h1>
 					<br/>
